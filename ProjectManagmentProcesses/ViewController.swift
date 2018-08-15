ee4e8e63c02d9cc80ddc13a9de5ec35b12cb45c2
Hector Andres Perez Villatoro <hectorandresperezvillatoro@hectors-macbook-pro.local>
2018-08-15 15:48:28 -0700

    Initial Commit

---
+++ ProjectManagmentProcesses/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectManagmentProcesses
+//
+//  Created by Hector Andres Perez Villatoro on 2018-08-15.
+//  Copyright © 2018 Hector Andres Perez Villatoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
